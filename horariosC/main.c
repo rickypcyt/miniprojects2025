cb92e120ace78d69ab1507a6d13741b9018e826a
rickypcyt <105986682+rickypcyt@users.noreply.github.com>
2025-04-10 19:29:08 +0200

    First commit

---
+++ horariosC/main.c
@@ -0,0 +1,14 @@
+// C program that shows a desktop notification when it's time to go to the gym to eat to turn off
+// the screen and sleep.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char mensaje = hola;
+
+int main() {
+    printf("");
+    if (mensaje == NULL) {
+        printf("No hay mensaje que printear\n");
+    }
+}
