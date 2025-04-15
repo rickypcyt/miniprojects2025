77aff67e8e9377549b42b3d54dd7ea1e902ecfbc
rickypcyt <105986682+rickypcyt@users.noreply.github.com>
2025-04-15 10:39:16 +0200

    Orden

--- algoritmos/prim.py
+++
@@ -1,2 +0,0 @@
-#hacer el algoritmo de primm desde 0.
-#basate en la logica de los arboles y los elementos de poder del mago y eso que tienes en obsidian.
--- algoritmos/sortingarray.cpp
+++
@@ -1,2 +0,0 @@
-// tienes un array por ejemplo {5, 3, 1, 4, 2}
-// n seria el tamano del array lo haces dividiendo el
