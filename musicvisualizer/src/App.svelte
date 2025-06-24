66004e69f614633aeecce6e2f0b3fee8eb87f60d
rickypcyt <105986682+rickypcyt@users.noreply.github.com>
2025-06-24 05:09:55 +0200

    Svelte prototype

---
+++ musicvisualizer/src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  let count: number = 0;
+</script>
+
+<main>
+  <h1>Hello Svelte + TypeScript + Bun</h1>
+  <button on:click={() => count++}>
+    Clicks: {count}
+  </button>
+</main> 
